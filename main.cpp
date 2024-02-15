5c19dd726ba6d1f9e679146ec70e956a7db2d36e
Dish3s <131878820+Dish3s@users.noreply.github.com>
2024-02-15 15:59:58 +0100

    [wip] yes

--- main.cpp
+++ main.cpp
@@ -5,15 +5,48 @@ using namespace std;
 
 int main() {
 
-    
-    cout << "Hello World" << endl;
+    int screenWidth = 1800;
+    int screenHeight = 1000;
 
-    InitWindow(300, 300, "My first Raylib window!");
+    int ballX = screenWidth / 2;
+    int ballY = screenHeight / 2;
+
+    int ballRadius = 10;
+
+    int ballSpeedX = 8;
+    int ballSpeedY = 8;
+
+    bool IsKeyPressed(int key);             // Check if a key has been pressed once
+    bool IsKeyDown(int key);                // Check if a key is being pressed
+    bool IsKeyReleased(int key);            // Check if a key has been released once
+    bool IsKeyUp(int key);                  // Check if a key is NOT being pressed
+    void SetExitKey(int key);               // Set a custom key to exit program (default is ESC)
+    int GetKeyPressed();                    // Get key pressed (keycode), call it multiple times for keys queued, returns 0 when queue is empty 
+    int GetCharPressed();                   // Get char pressed (unicode), call it multiple times for chars queued, returns 0 when the queue is empty
+
+
+
+    InitWindow(screenWidth, screenHeight, "My first Raylib window!");
     SetTargetFPS(60);
 
+    Font ft = LoadFont("resources/fonts/romulus.png");
+
     while (!WindowShouldClose()) {
+        ballX += ballSpeedX;
+        ballY += ballSpeedY;
+
+        if ((ballX + ballRadius >= screenWidth) || (ballX - ballRadius <= 0)) {
+            ballSpeedX = -ballSpeedX;
+        }
+        if ((ballY + ballRadius >= screenHeight) || (ballY - ballRadius <= 0)) {
+            ballSpeedY = -ballSpeedY;
+        }
+
         BeginDrawing();
-        ClearBackground(DARKGREEN);
+        ClearBackground(BLACK);
+        DrawTextEx(ft, "Let's smack some balls :)", Vector2{ 720, 50 }, 30, 2, WHITE);
+        DrawCircle(ballX, ballY, ballRadius, RED);
+        DrawLineStrip(Vector2 *, WHITE);
         EndDrawing();
     }
 
